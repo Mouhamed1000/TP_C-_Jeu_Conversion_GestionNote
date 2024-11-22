d91c44cb57989138e901394da87a5c4589586591
Mouhamed1000 <ndoyenaby12@gmail.com>
2024-11-22 22:35:46 +0000

    gestion note etudiant c#

---
+++ Etudiant.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace UsageCollections
+{
+    public class Etudiant
+    {
+        private string Nom;
+        private double NoteCC;
+        private double NoteDevoir;
+        public string nom { get; set; }
+        public double noteCC { get; set; }
+        public double noteDevoir { get; set; }
+
+        SortedList liste1 = new SortedList();
+
+        public int Age { get; set; }
+
+        public Etudiant () {}
+        public Etudiant (string nom, double noteCC, double noteDevoir)
+        {
+            Nom = nom;
+            NoteCC = noteCC;
+            NoteDevoir = noteDevoir;
+        }
+
+        // Trie par somme des notes avec un Comparer
+        public class ComparerEtudiantParNote : IComparer<Etudiant>
+        {
+            public int Compare(Etudiant x, Etudiant y)
+            {
+                double totalX = x.NoteCC + x.NoteDevoir;
+                double totalY = y.NoteCC + y.NoteDevoir;
+                return totalX.CompareTo(totalY);  
+            }
+        }
+
+        //Une méthode ToString() pour afficher les infos de l'etudiant
+        public override string ToString()
+        {
+            double moyenne = (NoteCC * (33.0/100) ) + (NoteDevoir * (67.0/100));
+            return $"Nom: {Nom} , Note CC: {NoteCC} , Note Devoir: {NoteDevoir} , Moyenne: {moyenne} ";
+        }
+
+        public SortedList AddStudents()
+        {
+            Console.WriteLine("Bienvenue dans le menu d'enregistrements des infos des étudiants");
+            Console.WriteLine("Combien d'étudiants voulez-vous enregistrer");
+            int nbreEtud = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i<= nbreEtud; i++)
+            {
+
+                Console.WriteLine("Veuillez entrer le nom d'un étudiant : ");
+                Nom = Console.ReadLine();
+
+                Console.WriteLine("Veuillez entrer la note CC d'un étudiant : ");
+                bool verite1 = Double.TryParse(Console.ReadLine(), out NoteCC);
+
+                while(verite1 == false)
+                {
+                    Console.WriteLine("La valeur saisie doit être un nombre entier ou réel (x) ou (x,y)");
+                    Console.WriteLine("Veuillez entrez un nombre");
+                    verite1 = Double.TryParse(Console.ReadLine(), out NoteCC) ;
+                }
+
+                Console.WriteLine("Veuillez entrer la note Devoir d'un étudiant : ");
+                bool verite2 = Double.TryParse(Console.ReadLine(), out NoteDevoir);
+
+                while(verite2 == false)
+                {
+                    Console.WriteLine("La valeur saisie doit être un nombre entier ou réel (x) ou (x,y)");
+                    Console.WriteLine("Veuillez entrez un nombre");
+                    verite2 = Double.TryParse(Console.ReadLine(), out NoteDevoir) ;
+                }
+
+                var etudiant = new Etudiant(Nom, NoteCC, NoteDevoir);
+
+                liste1.Add(Nom, etudiant);
+
+                Console.WriteLine(Nom + " enregistré !");
+
+            }
+            return liste1;
+
+        }
+
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UsageCollections;
+
+namespace UsageCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int reponse = 10;
+            SortedList liste2;
+            //Debut
+            Etudiant e1 = new Etudiant();
+            liste2 = e1.AddStudents();
+            while (!reponse.Equals(3) )
+            {
+                Console.WriteLine("Menu");
+                //Permettre d'afficher le nom, les notes et la moyenne d'un étudiant
+                Console.WriteLine("1. Voulez-vous afficher le nom, les notes et la moyenne d'un étudiant de la classe");
+                //Permettre de terminer
+                Console.WriteLine ("2. Souhaitez-vous terminer le programme");
+                 //Infos de tous les étudiants
+                Console.WriteLine("3. Voulez-vous afficher le nom, les notes et la moyenne de tous les étudiants de la classe");
+
+                reponse = Convert.ToInt32(Console.ReadLine());
+
+                if (reponse.Equals("3"))
+                {
+                    break;
+                }
+                else
+                {
+                    if (reponse.Equals (1))
+                    {
+                        AfficherUn(liste2);
+                    }
+                    if (reponse.Equals(2))
+                    {
+                        afficherTout(liste2);
+                    }
+                    while ( (!(reponse.Equals(1))) && (!(reponse.Equals(1))) && (!(reponse.Equals(1))) )
+                    {
+                        Console.WriteLine("Saisissez un nombre entre entre 1 et 3");
+                        reponse = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
+            } 
+        }
+
+        public static void AfficherUn(SortedList liste)
+        {
+            Console.WriteLine("Veuillez entrer le nom d'un étudiant");
+            string unEtudiant = Console.ReadLine();
+            Console.WriteLine("-----------Infos d'un étudiant de la classe-----------");
+            foreach (DictionaryEntry etudiant in liste)
+            {
+                Etudiant e3 = (Etudiant)etudiant.Value;
+                if (e3 != null && e3.nom != null  && e3.nom.Equals(unEtudiant))
+                {
+                    Console.WriteLine(e3);
+                }
+            }
+        }
+        public static void afficherTout(SortedList liste)
+        {
+            Console.WriteLine("-----------Infos des étudiants de la classe-----------");
+            foreach (DictionaryEntry etudiant in liste)
+            {
+                Etudiant e2 = (Etudiant)etudiant.Value;
+                Console.WriteLine(e2);
+            }
+        }
+
+    }
+
+}
